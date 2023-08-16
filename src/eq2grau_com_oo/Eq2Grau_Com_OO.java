8de914a7f776e1a0e60534c1c6638485444754a1
wallace <11222101515@alunos.umc.br>
2023-08-15 21:25:48 -0300

    commit_Scanner

--- src/eq2grau_com_oo/Eq2Grau_Com_OO.java
+++ src/eq2grau_com_oo/Eq2Grau_Com_OO.java
@@ -1,24 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package eq2grau_com_oo;
+
+import java.util.Scanner;
 
 public class Eq2Grau_Com_OO {
 
     public static void main(String[] args) {
         Eq2Grau eq = new Eq2Grau();
-        
-        eq.setA(1);
-        eq.setB(-5);
-        eq.setC(6);
-        
+        Scanner scan = new Scanner(System.in);
+
+        double a, b, c;
+        System.out.println("Digite o valor de A:");
+        a = scan.nextDouble();
+
+        System.out.println("Digite o valor de B:");
+        b = scan.nextDouble();
+
+        System.out.println("Digite o valor de C:");
+        c = scan.nextDouble();
+
+        eq.setA(a);
+        eq.setB(b);
+        eq.setC(c);
+
+        // eq.setA(1);
+        // eq.setB(-5);
+        // eq.setC(6);
+
         eq.calcularDelta();
         eq.calcularX1();
         eq.calcularX2();
-        
+
         System.out.println("x1: " + eq.getX1());
         System.out.println("x2: " + eq.getX2());
     }
-    
+
 }
